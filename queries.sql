5130279d8da3686b4a29fcbe1265e5074d2a015d
Ray Wong <46xxrwongxx64@gmail.com>
2026-03-03 17:22:45 -0800

    queries file

---
+++ queries.sql
@@ -0,0 +1,21 @@
+
+
+-- List date, comment, and bill for all closed requests with bill lower than 100
+
+-- List the customers that have paid less than 100 dollars for repairs based on their previous service requests.
+
+-- List first and last name of customers having more than 20 different cars
+
+-- Find how many cars each customer has counting from the ownership relation and discover who has more than 20 cars.
+
+-- List Make, Model, and Year of all cars build before 1995 having less than 50000 miles
+
+-- Get the odometer from the service_requests and find all cars before 1995 having less than 50000 miles in the odometer.
+
+-- List the make, model and number of service requests for the first k cars with the highest number of service orders.
+
+-- Find for all cars in the database the number of service requests. Return the make, model and number of service requests for the cars having the k highest number of service requests. The k value should be positive and larger than 0. The user should provide this value. Focus on the open service requests.
+
+-- List the first name, last name and total bill of customers in descending order of their total bill for all cars brought to the mechanic.
+
+-- For all service requests find the aggregate cost per customer and order customers according to that cost. List their first, last name and total bill
